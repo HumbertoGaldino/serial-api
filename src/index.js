ae6cc8a005bb2bda36cccfc755f68d218442d70a
Humberto Galdino <humberto.galdino@live.com>
2025-01-08 17:16:02 -0300

    Configurando o Swagger para documentação da API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "dotenv/config";
 
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import schemasSwagger from "./schemas.json" with { type: "json" };
+
 import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 
@@ -7,6 +11,26 @@ const app = express();
 app.use(express.json());
 
 app.use("/usuarios", usersRoutes);
+
+//Documentação da API
+var swaggerDefinition = {
+  info: {
+    title: "Serial API",
+    version: "1.0.0",
+    description: "Documentação da API do projeto Serial.",
+  },
+  components: {
+    schemas: schemasSwagger,
+  },
+};
+
+var options = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ["src/routes/*.js"],
+};
+
+var swaggerSpec = swaggerJSDoc(options);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 const port = process.env.PORT;
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -19,6 +19,68 @@ const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 //Cadastro
+
+/**
+ * @swagger
+ * /usuarios/cadastro:
+ *   post:
+ *     tags:
+ *       - User
+ *     description: Método para cadastro de usuários
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: user
+ *         description: Dados para criação de novo usuário
+ *         schema:
+ *           type: object
+ *           required:
+ *             - email
+ *             - name
+ *             - username
+ *             - password
+ *           properties:
+ *             email:
+ *               type: string
+ *               example: user@example.com
+ *             name:
+ *               type: string
+ *               example: John
+ *             username:
+ *               type: string
+ *               example: john_doe
+ *             password:
+ *               type: string
+ *               example: mysecurepassword
+ *     responses:
+ *       201:
+ *         description: Usuário cadastrado com sucesso!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Requisição inválida devido a problemas com o nome de usuário ou e-mail.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Este nome de usuário já está em uso.
+ *       500:
+ *         description: Erro no servidor, tente novamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Ocorreu um erro inesperado.
+ */
 router.post("/cadastro", async (req, res) => {
   try {
     const { email, name, username, password } = req.body;
@@ -37,11 +99,88 @@ router.post("/cadastro", async (req, res) => {
 
     res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
   } catch (error) {
+    if (error.code === "P2002" && error.meta.target.includes("username")) {
+      res.status(400).json({
+        message: "Este nome de usuário já está em uso. Escolha outro.",
+      });
+    }
+    if (error.code === "P2002" && error.meta.target.includes("email")) {
+      res.status(400).json({ message: "Este e-mail já foi cadastrado!" });
+    }
     res.status(500).json({ message: "Erro no servidor, tente novamente" });
   }
 });
 
 //Login
+
+/**
+ * @swagger
+ * /usuarios/login:
+ *   post:
+ *     tags:
+ *       - User
+ *     description: Login de usuário
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: user
+ *         description: Dados para login de usuário
+ *         schema:
+ *           type: object
+ *           required:
+ *             - email
+ *             - password
+ *           properties:
+ *             email:
+ *               type: string
+ *               example: user@example.com
+ *             password:
+ *               type: string
+ *               example: mysecurepassword
+ *     responses:
+ *       200:
+ *         description: Token gerado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *       400:
+ *         description: Senha inválida!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Senha inválida!"
+ *       404:
+ *         description: Usuário não encontrado!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Usuário não encontrado!"
+ *       500:
+ *         description: Erro no servidor, tente novamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Erro no servidor, tente novamente."
+ */
+
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
